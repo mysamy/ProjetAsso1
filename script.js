75e5ad1828432aa0019cb2b1a522450d442ef124
mysamy <47317673+mysamy@users.noreply.github.com>
2024-12-16 14:06:04 +0100

    modal

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const textInput = document.getElementById("textInput");
-const submitButton = document.getElementById("submitButton");
-const outputDiv = document.getElementById("outputDiv");
+(function handleTextInput() {
+  const textInput = document.getElementById("textInput");
+  const submitButton = document.getElementById("submitButton");
+  const outputDiv = document.getElementById("outputDiv");
 
-submitButton.addEventListener("click", function () {
-  
-  outputDiv.textContent = textInput.value;
-  outputDiv.style.display = 'block';
-  
-  
+  if (submitButton) {
+    submitButton.addEventListener("click", function () {
+      outputDiv.textContent = textInput.value;
+      outputDiv.style.display = "block";
+    });
+  }
+})();
+
+
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("myModal");
+
+// Ouvrir la modale
+openModalBtn.addEventListener("click", () => {
+    modal.showModal(); // Affiche la modale
 });
 
+// Fermer la modale
+closeModalBtn.addEventListener("click", () => {
+    modal.close(); // Ferme la modale
+});
